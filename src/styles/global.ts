6eafa84dce4e124fd9c202f8313718c1d15211de
John Rômulo <johnromulo@MacBook-Air-de-John.local>
2020-08-02 21:09:44 -0300

    config

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -24,7 +24,7 @@ export default createGlobalStyle`
 
   html, body, #root {
     height: 100%;
-    background : ${props => props.theme.colors.backgroudPrimay};
+    background : #EEEEEE;
   }
 
   body {
